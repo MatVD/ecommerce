4b0fd29023478ceaf288c0524af45b07dfb1bbea
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2022-10-14 19:23:03 +0200

    add all components

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">{["Product 1", "Product 2"].map((product) => product)}</div>
     
-      Footer
+      <FooterBanner />
     </>
   );
 };
